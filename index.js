e7a081bf459a6c2ca002214402e13acef41fd42a
ayathree <nobanitaayathree333@gmail.com>
2024-08-31 21:04:37 +0600

    create profile route

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ycbv1lf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,27 +21,52 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    // Connect the client to the server
+    await client.connect();
+    const userCollection = client.db('jobDB').collection('users');
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      try {
+        const user = req.body;
+        // don't let a user insert into the db if it already exists
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null });
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Error creating user', error: error.message });
+      }
+    });
+
+    app.get('/userResume/:email', async(req,res)=>{
+      const query = {email: req.params.email}
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // Ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    // You may choose to close the client connection or leave it open
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req,res)=>{
-    res.send('kudosware running')
+app.get('/', (req, res) => {
+  res.send('kudosware running');
 });
 
-app.listen(port, ()=>{
-    console.log(`kudosware is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`kudosware is running on port: ${port}`);
+});
